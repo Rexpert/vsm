1a67e26a86c576429432a05ac0c01c77af5aa4cc
Rexpert <liang2003147@gmail.com>
2022-09-18 16:30:17 +0800

    :goal_net: catching fail scrape

--- fb_fail_scraper.py
+++ fb_fail_scraper.py
@@ -74,9 +74,23 @@ def scrape(url):
             except:
                 continue
         return pd.DataFrame(dict(original_request_url=url, time=post_time, reaction_count=like, scrape=now), index=[0])
-    except:
+    except Exception as e:
+        save_screenshot(driver, 'screenshot.png')
+        raise e
         return pd.DataFrame(columns=['original_request_url'])
 
+
+def save_screenshot(driver: Chrome, path: str = '/tmp/screenshot.png') -> None:
+    # Ref: https://stackoverflow.com/a/52572919/
+    original_size = driver.get_window_size()
+    required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
+    required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+    driver.set_window_size(required_width, required_height)
+    # driver.save_screenshot(path)  # has scrollbar
+    driver.find_element_by_tag_name('body').screenshot(path)  # avoids scrollbar
+    driver.set_window_size(original_size['width'], original_size['height'])
+
+save_screenshot(driver, 'screenshot.png')
 
 def scrape_all(urls):
     driver.get(urls[0])
