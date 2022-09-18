c75fee6331de0e31a1cd194daf05f6f56f4d00f0
Rexpert <liang2003147@gmail.com>
2022-09-18 17:23:48 +0800

    :bug: Debug unable login

--- fb_fail_scraper.py
+++ fb_fail_scraper.py
@@ -75,20 +75,16 @@ def scrape(driver, url):
                 continue
         return pd.DataFrame(dict(original_request_url=url, time=post_time, reaction_count=like, scrape=now), index=[0])
     except Exception as e:
-        save_screenshot(driver, r'./screenshot.png')
-        raise e
         return pd.DataFrame(columns=['original_request_url'])
 
 
 def save_screenshot(driver: Chrome, path: str = '/tmp/screenshot.png') -> None:
     # Ref: https://stackoverflow.com/a/52572919/\
-    raise Exception
     original_size = driver.get_window_size()
     required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
     required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
     driver.set_window_size(required_width, required_height)
     # driver.save_screenshot(path)  # has scrollbar
-    Path(path).unlink()
     driver.find_element_by_tag_name('body').screenshot(path)  # avoids scrollbar
     driver.set_window_size(original_size['width'], original_size['height'])
 
@@ -96,12 +92,14 @@ def save_screenshot(driver: Chrome, path: str = '/tmp/screenshot.png') -> None:
 def scrape_all(driver, urls):
     driver.get(urls[0])
     time.sleep(2)
+    save_screenshot(driver, r'./screenshot.png')
     password = driver.find_elements_by_css_selector('input[name="pass"]')
     if len(password) > 0:
         password[0].send_keys(os.environ['PASSWORD'])
         password[0].send_keys(Keys.ENTER)
         time.sleep(5)
     results = []
+    save_screenshot(driver, r'./screenshot1.png')
     for url in urls:
         results.append(scrape(driver, url))
     return results
